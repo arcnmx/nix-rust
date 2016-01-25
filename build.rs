6dba8173a6d02dcbca8eee748f7e0db7bc0b6e06
arcnmx <arcnmx@users.noreply.github.com>
2016-01-25 18:48:09 -0500

    build script to determine NixString capabilities

---
+++ build.rs
@@ -0,0 +1,12 @@
+extern crate rustc_version;
+use rustc_version::version_matches;
+
+fn main() {
+	if version_matches(">= 1.3.0") {
+        println!("cargo:rustc-cfg=std_has_cstr_toowned");
+	}
+
+	if version_matches("< 1.7.0") {
+        println!("cargo:rustc-cfg=feature=nixstring");
+	}
+}
--- src/nix_string.rs
+++ src/nix_string.rs
@@ -1,47 +1,59 @@
-use std::ffi::{CStr, CString};
-use std::borrow::Cow;
+use std::ffi::CStr;
 
 /// Represents a type that can be converted to a `&CStr` without fail.
 ///
-/// Note: this trait exists in place of `AsRef<CStr>` because is not
-/// implemented until Rust 1.7.0
+/// Note: this trait is superceded by `AsRef<CStr>` in Rust 1.7.0
 pub trait NixString {
-    fn as_ref(&self) -> &CStr;
+	fn as_ref(&self) -> &CStr;
 }
 
-impl<'a> NixString for Cow<'a, CStr> {
-    fn as_ref(&self) -> &CStr {
-        self
-    }
+#[cfg(feature = "nixstring")]
+mod imp {
+	use std::ffi::{CStr, CString};
+	use super::NixString;
+
+	#[cfg(std_has_cstr_toowned)]
+	impl<'a> NixString for ::std::borrow::Cow<'a, CStr> {
+		fn as_ref(&self) -> &CStr {
+			self
+		}
+	}
+
+	impl NixString for CStr {
+		fn as_ref(&self) -> &CStr {
+			self
+		}
+	}
+
+	impl NixString for CString {
+		fn as_ref(&self) -> &CStr {
+			self
+		}
+	}
+
+	impl<'a, T: ?Sized + NixString> NixString for &'a T {
+		fn as_ref(&self) -> &CStr {
+			NixString::as_ref(*self)
+		}
+	}
+
+	impl<'a, T: ?Sized + NixString> NixString for &'a mut T {
+		fn as_ref(&self) -> &CStr {
+			NixString::as_ref(*self)
+		}
+	}
+
+	#[macro_export]
+	macro_rules! cstr {
+		($s:expr) => {
+			unsafe { ::std::ffi::CStr::from_ptr(concat!($s, "\0").as_ptr() as *const _) }
+		}
+	}
 }
 
-impl NixString for CStr {
-    fn as_ref(&self) -> &CStr {
-        self
-    }
-}
-
-impl NixString for CString {
-    fn as_ref(&self) -> &CStr {
-        self
-    }
-}
-
-impl<'a, T: ?Sized + NixString> NixString for &'a T {
-    fn as_ref(&self) -> &CStr {
-        NixString::as_ref(*self)
-    }
-}
-
-impl<'a, T: ?Sized + NixString> NixString for &'a mut T {
-    fn as_ref(&self) -> &CStr {
-        NixString::as_ref(*self)
-    }
-}
-
-#[macro_export]
-macro_rules! cstr {
-    ($s:expr) => {
-        unsafe { ::std::ffi::CStr::from_ptr(concat!($s, "\0").as_ptr() as *const _) }
-    }
+#[cfg(not(feature = "nixstring"))]
+impl<T: AsRef<CStr>> NixString for T {
+	fn as_ref(&self) -> &CStr {
+		return AsRef::as_ref(self)
+	}
 }
